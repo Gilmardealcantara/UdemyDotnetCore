975b8f49caa8f9a48c58989280374d8b483c9090
Gilmar <gilmar.alcantara@ibm.com>
2020-04-04 21:02:40 -0300

    Add Data

--- src/ITDeveloper.Data/ORM/ITDeveloperDbContext.cs
+++ src/ITDeveloper.Data/ORM/ITDeveloperDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using Microsoft.EntityFrameworkCore;
+
 namespace ITDeveloper.Data.ORM
 {
-    public class ITDeveloperDbContext
+    public class ITDeveloperDbContext: DbContext
     {
-        public ITDeveloperDbContext()
+        public ITDeveloperDbContext(DbContextOptions<ITDeveloperDbContext> options)
+            :base(options)
         {
         }
     }
