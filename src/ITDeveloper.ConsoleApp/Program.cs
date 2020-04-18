ce0b63ab14daa8119647529aac5de10c55cf88dd
Gilmar <gilmar.alcantara@ibm.com>
2020-04-18 19:39:01 -0300

    Add console app

---
+++ src/ITDeveloper.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITDeveloper.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
