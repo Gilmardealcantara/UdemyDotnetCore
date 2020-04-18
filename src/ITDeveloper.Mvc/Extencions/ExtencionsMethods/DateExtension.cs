ec0e1ffc2897a0d5787831729828f7940cccc8e5
Gilmar <gilmar.alcantara@ibm.com>
2020-04-18 18:59:40 -0300

    Add Date Extension Method

--- src/ITDeveloper.Mvc/Extencions/ExtencionsMethods/DateExtension.cs
+++ src/ITDeveloper.Mvc/Extencions/ExtencionsMethods/DateExtension.cs
@@ -1,5 +1,12 @@
+using System;
 namespace ITDeveloper.Mvc.Extensions.ExtensionsMethods {
     public static class DateExtension {
+        public static string ToBrazilianDate(this DateTime value) {
+            return value.ToString(format: "dd/MM/yyyy");
+        }
 
+        public static string ToBrazilianDateTime(this DateTime value) {
+            return value.ToString(format: "dd/MM/yyyy hh:MM:ss");
+        }
     }
 }
